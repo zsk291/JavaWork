6d2019667005b13cb5e35e8daf7fd4e6f2397d5d
“zsk” <3057423823@qq.com>
2022-09-25 11:57:58 +0800

    JavaWork

---
+++ src/work_4_leetcode67_119_234_1018/extendDemo1/TestExtends.java
@@ -0,0 +1,24 @@
+package work_4_leetcode67_119_234_1018.extendDemo1;
+
+class Student {
+    String name;
+    public void speak(){//定义一个说话的方法
+        System.out.println("我是长江大学的学生");
+    }
+}
+//定义一个软件系学生的继承学生类
+class SoftStudent extends Student {
+    //定义一个学习方法，可以获取父类属性
+    public void learn(){
+        System.out.println("我是"+name+",我正在学习Java");
+    }
+}
+
+public class TestExtends {
+    public static void main(String[] args) {
+        SoftStudent ss = new SoftStudent();//创建一个SoftStudent类对象
+        ss.name = "张三";//为SoftwareStudent中的name属性赋值
+        ss.speak();//调用从父类继承的speak()方法
+        ss.learn();//调用自己的learn()方法
+    }
+}
